38f926c8f82a8144e8b3632b676750b020189a12
philzin <felippe.f.andrade@gmail.com>
2021-01-07 17:30:03 -0300

    mpv trackma tagmasks

--- config.def.h
+++ config.def.h
@@ -44,6 +44,8 @@ static const Rule rules[] = {
 	{ NULL,      NULL,     "雀魂Plus",     1<<6,      0,          0,           0,        -1 },
 	{ NULL,      NULL,     "Majsoul Plus", 1<<6,      0,          0,           0,        -1 },
 	{ NULL,      NULL,     "Transmission Remote", 1<<2, 0,        0,           0,        -1 },
+	{ "mpv",     NULL,     NULL,           1<<1,      0,          0,           0,        -1 },
+	{ NULL,      NULL,     "Trackma",      1<<2,      0,          0,           0,        -1 },
 };
 
 /* layout(s) */
