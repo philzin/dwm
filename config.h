19897a6e24e739f772f3eaec152792273e1cf162
philzin <felippe.f.andrade@gmail.com>
2021-01-23 18:04:38 -0300

    pixelsize=12

--- config.h
+++ config.h
@@ -16,8 +16,8 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 20;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:pixelsize=14", "Noto Color Emoji:pixelsize=14:antialias=true" };
-static const char dmenufont[]       = "monospace:pixelsize=14";
+static const char *fonts[]          = { "monospace:pixelsize=12", "Noto Color Emoji:pixelsize=12:antialias=true" };
+static const char dmenufont[]       = "monospace:pixelsize=12";
 static const char col_gray1[]       = "#1d1f21";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#c5c8c6";
