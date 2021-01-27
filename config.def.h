76afff78717b0e8105de6a5aa71017997ce26d1c
philzin <felippe.f.andrade@gmail.com>
2021-01-27 16:22:38 -0300

    siji

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 14;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:pixelsize=12", "Cozette", "Noto Color Emoji:pixelsize=10" };
+static const char *fonts[]          = { "Siji", "monospace:pixelsize=12", "Noto Color Emoji:pixelsize=10" };
 static const char dmenufont[]       = "monospace:pixelsize=12";
 static const char col_gray1[]       = "#1d1f21";
 static const char col_gray2[]       = "#444444";
@@ -60,9 +60,9 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "⊞",      tile },    /* first entry is default */
-	{ "",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
@@ -87,7 +87,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-i", NULL };
-static const char *termcmd[]  = { "urxvtc", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 static const char *browsercmd[]  = { "chromium", NULL };
 static const char *disccmd[]  = { "discord", NULL };
 static const char *upvol[]   = { "pamixer", "-i", "5", NULL };
